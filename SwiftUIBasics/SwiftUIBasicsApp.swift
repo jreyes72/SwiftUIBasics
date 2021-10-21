8ee63bed2553d9ca0c07805431dc83e3195652ba
mia madrigal <madrigalmia14@gmail.com>
2021-10-20 21:54:06 -0500

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Central States SER Mac 19 on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
